a017e8a576d6e228dab5483090e223ae81c4bda4
nielskrikke <nielskrikkeon@gmail.com>
2026-07-15 12:26:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 4000,
-        host: '127.0.0.1',
+        host: '0.0.0.0',
         hmr: false,
       },
       plugins: [react()],
